2ecd899b79dc87e550b7cf7755331f0a4ac3a7bd
zhaozhipeng <zhaozp15@tsinhua.org.cn>
2021-06-18 11:12:06 +0800

    add multifea13_1_c2_prelu_from_b128x8_step2

---
+++ configs/zzp/multifea/multifea13_1_c2/multifea13_1_c2_prelu_from_b128x8_step2.py
@@ -0,0 +1,36 @@
+_base_ = [
+    '../../../_base_/datasets/imagenet_bs128.py',
+    '../../../_base_/schedules/imagenet_bs1024.py', '../../../_base_/default_runtime.py'
+]
+
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='MultiFea',
+        arch='mf13_1_3',
+        binary_type=(True, True),
+        stem_act='prelu',
+        block_act=('prelu', 'prelu_pi=1'),
+        stem_channels=64,
+        base_channels=90,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch',),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=1000,
+        in_channels=720,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
+
+# set weight_decay to 0
+optimizer = dict(
+    weight_decay=0
+)
+
+load_from = 'work_dir/multifea/multifea13_1/multifea13_1_c2/multifea13_1_c2_b128x8_step1/epoch_100.pth'
+work_dir = 'work_dir/multifea/multifea13_1/multifea13_1_c2/multifea13_1_c2_prelu_from_b128x8_step2'
+find_unused_parameters=False
+seed = 166
