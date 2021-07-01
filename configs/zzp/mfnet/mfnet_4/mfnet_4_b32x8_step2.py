879a4e96f44d7854b055c8b13d0aae1c2a391a3d
zhaozhipeng <zhaozp15@tsinhua.org.cn>
2021-07-01 11:36:20 +0800

    add mfnet_4

---
+++ configs/zzp/mfnet/mfnet_4/mfnet_4_b32x8_step2.py
@@ -0,0 +1,19 @@
+_base_ = [
+    './mfnet_4_b32x8_step1.py'
+]
+
+model = dict(
+    backbone=dict(
+        binary_type=(True, True),
+))
+
+optimizer = dict(
+    type='Adam',
+    lr=5e-4,
+    weight_decay=0.0,
+    paramwise_cfg=dict(norm_decay_mult=0)
+)
+
+
+load_from = 'work_dir/mfnet/mfnet_4/mfnet_4_b32x8_step1/epoch_100.pth'
+work_dir = 'work_dir/mfnet/mfnet_4/mfnet_4_b32x8_step2'
