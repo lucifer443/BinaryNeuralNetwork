863b0211fb48878667766997ee516af6f78a0e00
zhaozhipeng <zhaozp15@tsinhua.org.cn>
2021-07-16 17:14:02 +0800

    test 0716

---
+++ configs/zzp/mfnet/mfnet_5/mfnet_5_sgd/mfnet_5_wodprelu_step2.py
@@ -0,0 +1,16 @@
+_base_ = [
+    './mfnet_5_wodprelu_step1.py'
+]
+
+model = dict(
+    backbone=dict(
+        binary_type=(True, True),
+))
+
+optimizer = dict(
+    weight_decay=0.0,
+)
+
+
+load_from = 'work_dir/mfnet/mfnet_5/mfnet_5_sgd/mfnet_5_wodprelu_step1/epoch_100.pth'
+work_dir = 'work_dir/mfnet/mfnet_5/mfnet_5_sgd/mfnet_5_wodprelu_step2'
