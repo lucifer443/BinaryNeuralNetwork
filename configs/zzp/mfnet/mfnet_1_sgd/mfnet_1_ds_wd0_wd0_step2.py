bfe2fc8c579b79743aca7f6e8c637ad6ed2bab10
zhaozhipeng <zhaozp15@tsinhua.org.cn>
2021-07-19 19:11:54 +0800

    test 0719-1

---
+++ configs/zzp/mfnet/mfnet_1_sgd/mfnet_1_ds_wd0_wd0_step2.py
@@ -0,0 +1,16 @@
+_base_ = [
+    './mfnet_1_ds_wd0_wd0_step1.py'
+]
+
+model = dict(
+    backbone=dict(
+        binary_type=(True, True),
+))
+
+optimizer = dict(
+    weight_decay=0.0,
+)
+
+
+load_from = 'work_dir/mfnet/mfnet_1/mfnet_1_sgd/mfnet_1_ds_wd0_wd0_step1/epoch_100.pth'
+work_dir = 'work_dir/mfnet/mfnet_1/mfnet_1_sgd/mfnet_1_ds_wd0_wd0_step2'
