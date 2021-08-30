06db09c871825c030684971d7d7766d9605942b0
zhaozhipeng <zhaozp15@tsinhua.org.cn>
2021-08-30 16:52:05 +0800

    test 0830

---
+++ configs/zzp/multifea/multifea13_3_5_75/multifea13_3_5_75_b128x8_step1.py
@@ -0,0 +1,32 @@
+_base_ = [
+    '../../../_base_/datasets/imagenet_bs128.py',
+    '../../../_base_/schedules/imagenet_bs1024.py', '../../../_base_/default_runtime.py'
+]
+
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='MultiFea',
+        arch='mf_3_5',
+        binary_type=(True, False),
+        stem_act='prelu',
+        block_act=('prelu', 'identity'),
+        thres=(-0.75, 0.0, 0.75),
+        stem_channels=64,
+        base_channels=64,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch',),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=1000,
+        in_channels=512,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+        topk=(1, 5),
+    ))
+
+
+work_dir = 'work_dir/multifea/multifea13_3_5/multifea13_3_5_75/multifea13_3_5_75_b128x8_step1'
+find_unused_parameters=False
+seed = 166
